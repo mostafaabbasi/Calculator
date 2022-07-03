1e4ed1fb42f1976a935493ddcc655009bb5399db
Mostafa Abbasi <mostafaabbasisani@outlook.com>
2022-07-03 15:38:13 +0430

    change functionality

--- Calculator/Context.cs
+++ Calculator/Context.cs
@@ -2,14 +2,61 @@
 {
     public class Context
     {
+        #region Definitions
         private readonly ICalculator _calculator;
-        public Context(ICalculator calculator)
+        private double FirstNumber { get; set; }
+        private double SecondNumber { get; set; }
+        private string Operator { get; set; }
+        #endregion
+
+        #region Public
+        public Context(double firstNumber, double secondNumber, string oprt)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+            Operator = oprt;
+        }
+
+        public double Execute()
+        {
+            Context calaculator;
+            double result = 0.0;
+
+            switch (Operator)
+            {
+                case "+":
+                    calaculator = new Context(new Addition());
+                    result = calaculator.Execute(FirstNumber, SecondNumber);
+                    break;
+                case "-":
+                    calaculator = new Context(new Subtraction());
+                    result = calaculator.Execute(FirstNumber, SecondNumber);
+                    break;
+                case "/":
+                    calaculator = new Context(new Division());
+                    result = calaculator.Execute(FirstNumber, SecondNumber);
+                    break;
+                case "*":
+                    calaculator = new Context(new Multiplication());
+                    result = calaculator.Execute(FirstNumber, SecondNumber);
+                    break;
+                default:
+                    throw new Exception("Error occured");
+            }
+            return result;
+        }
+        #endregion
+
+        #region Private
+        private Context(ICalculator calculator)
         {
             _calculator = calculator;
         }
-        public double Execute(double x, double y)
+
+        private double Execute(double x, double y)
         {
             return _calculator.Calculate(x, y);
         }
+        #endregion
     }
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,32 +3,16 @@ string Operator = string.Empty;
 
 Console.Write("Enter first number : ");
 number1 = double.Parse(Console.ReadLine());
-Console.Write("Enter second number : ");
-number2 = double.Parse(Console.ReadLine());
+
 Console.Write("Enter one of these => +,-,/,* : ");
 Operator = Console.ReadLine();
 
-Context calaculator;
+Console.Write("Enter second number : ");
+number2 = double.Parse(Console.ReadLine());
 
-switch (Operator)
-{
-    case "+":
-        calaculator = new Context(new Addition());
-        result = calaculator.Execute(number1, number2);
-        break;
-    case "-":
-        calaculator = new Context(new Subtraction());
-        result = calaculator.Execute(number1, number2);
-        break;
-    case "/":
-        calaculator = new Context(new Division());
-        result = calaculator.Execute(number1, number2);
-        break;
-    case "*":
-        calaculator = new Context(new Multiplication());
-        result = calaculator.Execute(number1, number2);
-        break;
-    default:
-        throw new Exception("Error occured");
-}
+Context calaculator = new Context(number1, number2, Operator);
+
+result = calaculator.Execute();
+
+
 Console.WriteLine("Result : {0}", result);
